fe277c20da7711fdad3ad6616bab45ef7c3621fd
Kristinn <kris@Kristinns-MacBook-Air.local>
2023-03-25 17:33:07 -0400

    create functions to grab form and input values

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,3 +1,4 @@
+//Initial comments
 const comments = [
     {
     name: "Miles Acosta",
@@ -14,4 +15,26 @@ const comments = [
     date: "02/17/2021",
     comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
     }
-]
+] 
+
+//Grabbing the form element and post sections
+const form = document.querySelector('.conversation__form');
+const post = document.querySelector('.posts')
+
+//Function to run when form is submitted
+const formFunction = (event) => {
+    event.preventDefault();
+
+    const nameInput = document.querySelector('#name');
+    const commentInput = document.querySelector("#comment")
+
+    const postObject = {
+        name: nameInput.value,
+        date: new Date(),
+        comment: commentInput.value
+    };
+
+    comments.push(postObject);
+}
+
+form.addEventListener('submit', )
