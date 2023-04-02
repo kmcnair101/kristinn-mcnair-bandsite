41b1b17ba755ac6f7c5a2cca12c29bbf61d5768e
Kristinn <kris@kristinns-air.attlocal.net>
2023-04-02 17:52:34 -0400

    add axios to index-page js

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,30 +1,20 @@
+const apiKey = "d5f5c387-62f7-48ea-abf2-eec461dc3393";
+
 //Grabbing the form element and post sections
 const form = document.querySelector('.conversation__form');
 const posts = document.querySelector('.posts')
 
-//Initial comments
-const comments = [{
-    name: "Connor Walton", 
-    date: "02/17/2021",
-    comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-},
-{
-    name: "Emilie Beach",
-    date: "01/09/2021",
-    comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-},   
-{
-    name: "Miles Acosta",
-    date: "12/20/2020",
-    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-}] 
+let comments = [];
+
+
+
 
 //Takes comment and creates article with elements inside using innerHTML and template literals
 const displayComment = comment => {
     const post = document.createElement('article');
     post.className = "post";
     //Use the new Date on comment date to change the presented format
-    const newDate = new Date(comment.date);
+    const newDate = new Date(comment.timestamp);
     const commentDate = `${newDate.getMonth() + 1}/${newDate.getDate()}/${newDate.getFullYear()}`;
 
     post.innerHTML = `
@@ -47,27 +37,44 @@ const render = () => {
         displayComment(comment);
     }
 }
+
+axios
+    .get(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`)
+    .then(response => {
+        console.log(response);
+        comments = response.data;
+        render();
+})
+    .catch( error => {
+    console.log(error);
+})
+
 //Function to run when form is submitted
 const formFunction = event => {
     //prevents default action
     event.preventDefault();
     //Getting the elements
-    const nameInput = document.querySelector('#name');
-    const commentInput = document.querySelector("#comment")
+    const data = {
+        name: event.target.name.value,
+        comment: event.target.comment.value
+    };
     //creating an object
     const postObject = {
-        name: nameInput.value,
-        date: new Date(), 
-        comment: commentInput.value
+        name: data.value,
+        timestamp: Date.now(), 
+        comment: data.value
     };
-    //pushing the object to the start of the array of objects
-    comments.unshift(postObject);
-
+    axios.post(`https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`, data)
+        .then(response => {
+            console.log(response);
+            comments.unshift(response.data);
+            render();
+        })
+        .catch(error => {
+            console.log(error);
+        })
     //resetting the value
-    nameInput.value = "";
-    commentInput.value = "";
-    render();
-
+    event.target.reset();
 }
 //Event listener for form submission
 form.addEventListener('submit', formFunction)
